1176067d7d3a65c9560ae841a1f3cc10ee326fb6
Nandan17 <vgnandan02@gmail.com>
2022-03-23 23:52:35 +0530

    step-2

---
+++ lib/views/varify_email_view.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VarifyEmailView extends StatefulWidget {
+  const VarifyEmailView({ Key? key }) : super(key: key);
+
+  @override
+  State<VarifyEmailView> createState() => _VarifyEmailViewState();
+}
+
+class _VarifyEmailViewState extends State<VarifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Varify the email'),),
+      body: Column(
+        children: [
+          const Text('Please Varify your email address'),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              user?.sendEmailVerification();
+              },
+              child: Text('Send email varification'),
+           )
+        ],
+      ),
+    );
+  }
+}
