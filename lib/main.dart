640c08ce17887d32c00f5a4a0f549c1feabe5cfa
Nandan17 <vgnandan02@gmail.com>
2022-03-25 10:09:16 +0530

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/varify_email_view.dart';
-
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,15 +40,17 @@ class Homepage extends StatelessWidget {
              case ConnectionState.done:
                 final user = FirebaseAuth.instance.currentUser;
                 if(user != null){
-                  if(user.emailVerified){
-                    print('email varified');
-                  }else{
-                    return const VarifyEmailView();
-                  }
+                  //these are the lines to uncomment
+                  // if(user.emailVerified){
+                  //   print('email varified');
+                  //   return const NotesView();
+                  // }else{
+                  //   return const VarifyEmailView();
+                  // }
                 }else{
                     return const LoginView();
                 }
-                return const Text('DOne');
+                return const NotesView();
           //       print(user);
           //       if(user?.emailVerified ?? false){
           //         return const Text('Done');
@@ -66,7 +68,79 @@ class Homepage extends StatelessWidget {
   }
 }
 
+enum MenuAction { logout }
 
+
+class NotesView extends StatefulWidget {
+  const NotesView({ Key? key }) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async{
+              switch (value) {
+                
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  //logout from firebase
+                  if(shouldLogout){
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/',
+                       (_) => false,
+                       );
+                  }
+                  //devtools.log(shouldLogout.toString());
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text('Log out'),
+              ),
+              ];
+
+              
+            },)
+        ],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+
+Future<bool> showLogOutDialog(BuildContext context){
+    return showDialog<bool>(context: context, 
+    builder: (context){
+      return AlertDialog(
+          title: const Text('Sign out'),
+          content: const Text('Are you sure want to sign out'),
+          actions: [
+            TextButton(onPressed:() {
+              Navigator.of(context).pop(false);
+            }, child: const Text('Cancel')
+            ),
+            TextButton(onPressed:() {
+              Navigator.of(context).pop(true);
+            }, child: const Text('Log Out')
+            )
+          ],
+      );
+    },
+    ).then((value) => value ?? false);
+}
 
 //creating a stateless widget
 //scaffold - a basic building block we need a scaffold inside our Homepage widget
