9b9e31e27080cc1e043b5f9e3b8cea11c603746a
Nandan17 <vgnandan02@gmail.com>
2022-03-31 10:33:33 +0530

    step-13

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -4,3 +4,4 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const varifyEmailRoute = '/varify-email/';
+const newNoteRoute = '/notes/new-note/';
